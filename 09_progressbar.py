0a473a2d3be97954fc6b3eec8a7f3aa351c2793c
uberphoebus <uberphoebus@gmail.com>
2022-01-17 13:51:51 +0900

    intermed save

--- 09_progressbar.py
+++ 09_progressbar.py
@@ -7,18 +7,20 @@ root.geometry('320x240')
 
 # progressbar = ttk.Progressbar(root, maximum=100, mode='indeterminate')
 # indeterminate 결정되지 않음
-progressbar = ttk.Progressbar(root, maximum=100, mode='determinate')
-progressbar.start(10) # 10ms 마다 움직임
-progressbar.pack()
+# progressbar = ttk.Progressbar(root, maximum=100, mode='determinate')
+# progressbar.start(10) # 10ms 마다 움직임
+# progressbar.pack()
 
-
-
+p_var2 = DoubleVar()
+progressbar2 = ttk.Progressbar(root, maximum=100, length=150, variable=p_var2)
+progressbar2.pack()
 
 
 def btncmd():
+    progressbar2.stop() # 작동 중지
     pass
 
-btn = Button(root, text='order', command=btncmd)
+btn = Button(root, text='start', command=btncmd)
 btn.pack()
 
 root.mainloop()
