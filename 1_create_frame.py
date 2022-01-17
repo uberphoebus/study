13cd7706e7a29e318ca355c8ecb60c17b28b9aaa
uberphoebus <uberphoebus@gmail.com>
2022-01-17 11:16:13 +0900

    initial commit

---
+++ 1_create_frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title('nado gui')
+root.geometry('640x480')
+# root.geometry('640x480+100+300') # 가로 x 세로 + x + y
+
+root.resizable(False, False) # 높이, 너비 변경 불가
+
+root.mainloop() # 창이 닫히지 않는 루프
