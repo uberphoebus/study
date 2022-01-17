4a9c84afd3b738d72a04a6ca19c1346817acdec5
uberphoebus <uberphoebus@gmail.com>
2022-01-17 20:03:51 +0900

    intermed save [to 14_grid.py]

--- 09_progressbar.py
+++ 09_progressbar.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.ttk as ttk
+import time
 
 root = Tk()
 root.title('nado gui')
@@ -12,15 +13,18 @@ root.geometry('320x240')
 # progressbar.pack()
 
 p_var2 = DoubleVar()
-progressbar2 = ttk.Progressbar(root, maximum=100, length=150, variable=p_var2)
+progressbar2 = ttk.Progressbar(root, maximum=10, length=150, variable=p_var2)
 progressbar2.pack()
 
 
-def btncmd():
-    progressbar2.stop() # 작동 중지
-    pass
+def btncmd2():
+    for i in range(1, 11):
+        time.sleep(1)
+        p_var2.set(i) # 프로그레스 바의 값 설정
+        progressbar2.update() # ui 업데이트
+        print(p_var2.get())
 
-btn = Button(root, text='start', command=btncmd)
+btn = Button(root, text='start', command=btncmd2)
 btn.pack()
 
 root.mainloop()
